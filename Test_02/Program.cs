a6b29c1d864013afbff2470e3d1b0a62b40c8250
makeupova <makeupova@yandex.ru>
2020-06-02 16:42:43 +0300

    В роект Test_02 добавлено Изменение 01.

--- Test_02/Program.cs
+++ Test_02/Program.cs
@@ -7,6 +7,7 @@ namespace Test_02
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Изменение 01");
 
             Console.ReadKey();
         }
